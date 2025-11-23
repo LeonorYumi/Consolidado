f1b7a530335300922ed4dd6f2b7ae40610a820b3
LeonorYumi <Leonoryumi@gmail.com>
2025-11-23 01:28:13 -0500

    Ejercicio 2 Dispositivos electronicos

---
+++ src/DispositivosElectronicos/Telefono.java
@@ -0,0 +1,15 @@
+package DispositivosElectronicos;
+
+public class Telefono extends Dispositivo {
+    private double pulgadasPantalla;
+    public Telefono(String marca, String modelo, double precio, double pulgadasPantalla)
+            throws DatoInvalidoException {
+        super(marca, modelo, precio);
+        this.pulgadasPantalla = pulgadasPantalla;
+    }
+    @Override
+    public String mostrarInfo() {
+        return "Teléfono = " + super.mostrarInfo() +
+                ", Pantalla: " + pulgadasPantalla + " pulgadas";
+    }
+}
